86177cf84f4324c4892c9578a6551149715207c6
LA4AM12 <xingxiu.fyc@gmail.com>
2021-10-07 11:00:30 +0800

    Longest Common Subsequence -- medium

---
+++ java_v/src/dp/Solution1143.java
@@ -0,0 +1,30 @@
+package dp;
+
+import com.sun.source.tree.NewArrayTree;
+
+/**
+ * @author : LA4AM12
+ * @create : 2021-10-07 10:47:24
+ */
+public class Solution1143 {
+	public int longestCommonSubsequence(String text1, String text2) {
+		int m = text1.length(), n = text2.length();
+
+		int[] dp = new int[n+1];
+
+		for (int i = 1; i <= m; i++) {
+			int pre = 0;
+			for (int j = 1; j <= n; j++) {
+				int temp = dp[j];
+				if (text1.charAt(i-1) == text2.charAt(j-1)){
+					dp[j] = pre + 1;
+				}else {
+					dp[j] = Math.max(dp[j],dp[j-1]);
+				}
+				pre = temp;
+			}
+		}
+
+		return dp[n];
+	}
+}
