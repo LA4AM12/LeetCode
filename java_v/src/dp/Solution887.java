96d324617670a009e09b3f9ec1d9f493d57517bb
LA4AM12 <xingxiu.fyc@gmail.com>
2021-10-18 10:19:04 +0800

    Super Egg Drop -- hard

--- java_v/src/dp/Solution887.java
+++ java_v/src/dp/Solution887.java
@@ -4,36 +4,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution887 {
-    //动态规划 + 二分查找
-    Map<String, Integer> memo = new HashMap<>();
+	Map<Integer, Integer> memo = new HashMap<>();
 
-    public int superEggDrop(int k, int n) {
-        return dp(k, n);
-    }
+	public int superEggDrop(int k, int n) {
+		if (k == 1) return n;
+		if (n == 0) return 0;
 
-    //两个状态 ： 对应k个鸡蛋   n个可能的楼层位置
-    public int dp(int k, int n) {
-        if (!memo.containsKey(k + "," + n)) {
-            if (k == 1) return n;
-            if (n == 0) return 0;
-            int ans;
-            int lo = 1, hi = n;
-            while (lo + 1 < hi) {
-                int x = (lo + hi) >>> 1;     //在第x层扔鸡蛋
-                int t1 = dp(k - 1, x - 1);   //鸡蛋碎了   关于x的递增函数
-                int t2 = dp(k, n - x);        //鸡蛋没碎   关于x的递减函数
+		if (!memo.containsKey(100 * n + k)) {
+			int res = Integer.MAX_VALUE;
 
-                if (t1 < t2) {
-                    lo = x;
-                } else if (t1 > t2) {
-                    hi = x;
-                } else {
-                    lo = hi = x;
-                }
-            }
-            ans = Math.min(Math.max(dp(k - 1, lo - 1), dp(k, n - lo)), Math.max(dp(k - 1, hi - 1), dp(k, n - hi))) + 1;
-            memo.put(k + "," + n, ans);
-        }
-        return memo.get(k + "," + n);
-    }
+			// chose a floor
+			// for (int i = 1; i <= n + 1; i++) {
+			// 	res = Math.min(res, Math.max(superEggDrop(k - 1, i - 1), superEggDrop(k, n - i)) + 1);
+			// }
+
+			// binary search
+			int lo = 1, hi = n;
+			while (lo <= hi) {
+				int mid = (lo + hi) >> 1;
+				int broken = superEggDrop(k - 1, mid - 1);
+				int notBroken = superEggDrop(k, n - mid);
+				if (broken > notBroken) {
+					hi = mid - 1;
+					res = Math.min(res, broken + 1);
+				} else {
+					lo = mid + 1;
+					res = Math.min(res, notBroken + 1);
+				}
+			}
+
+			memo.put(100 * n + k, res);
+		}
+
+		return memo.get(100 * n + k);
+	}
 }
