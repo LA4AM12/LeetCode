1b9d59b18aadf73193ec52ffb9bb330a70d0d24b
LA4AM12 <xingxiu.fyc@gmail.com>
2021-10-25 10:03:14 +0800

    LRU Cache -- medium

--- java_v/src/DataStructure/LRUCache.java
+++ java_v/src/DataStructure/LRUCache.java
@@ -1,5 +1,6 @@
 package DataStructure;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -9,24 +10,81 @@ import java.util.Map;
  * @description : LRU Cache
  */
 
-class LRUCache extends LinkedHashMap<Integer,Integer> {
-	private final int capacity;
+class LRUCache {
+	class Entry {
+		int key, value;
+		Entry before, after;
 
-	@Override
-	protected boolean removeEldestEntry(Map.Entry eldest) {
-		return size() > this.capacity;
+		public Entry() {}
+
+		public Entry(int key, int value) {
+			this.key = key;
+			this.value = value;
+		}
 	}
 
+	private Map<Integer,Entry> cache = new HashMap<>();
+	private int size;
+	private int capacity;
+	private Entry head, tail;
+
 	public LRUCache(int capacity) {
-		super(capacity,0.75f,true);
+		this.size = 0;
 		this.capacity = capacity;
+
+		//dummy head & tail
+		this.head = new Entry();
+		this.tail = new Entry();
+		head.after = tail;
+		tail.before = head;
 	}
 
 	public int get(int key) {
-		return getOrDefault(key,-1);
+		Entry entry = cache.get(key);
+		if (entry == null)
+			return  -1;
+
+		//move to head
+		moveToHead(entry);
+
+		return entry.value;
 	}
 
 	public void put(int key, int value) {
-		super.put(key,value);
+		Entry entry = cache.get(key);
+		if (entry == null) {
+			Entry newEntry = new Entry(key, value);
+			cache.put(key,newEntry);
+
+			addToHead(newEntry);
+			++size;
+
+			if (size > capacity){
+				cache.remove(tail.before.key);
+				remove(tail.before);
+				--size;
+			}
+		}else {
+			entry.value = value;
+			moveToHead(entry);
+		}
+	}
+
+	private void moveToHead(Entry entry) {
+		remove(entry);
+		addToHead(entry);
+	}
+
+	private void remove(Entry entry) {
+		entry.before.after = entry.after;
+		entry.after.before = entry.before;
+	}
+
+	private void addToHead(Entry entry) {
+		entry.before = head;
+		entry.after = head.after;
+
+		head.after.before = entry;
+		head.after = entry;
 	}
 }
