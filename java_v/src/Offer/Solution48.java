bbdce97b279261754b7e9a3147f4421c81194c6d
LA4AM12 <xingxiu.fyc@gmail.com>
2021-10-25 11:04:28 +0800

    lengthOfLongestSubstring -- medium

--- java_v/src/Offer/Solution48.java
+++ java_v/src/Offer/Solution48.java
@@ -5,13 +5,17 @@ import java.util.Map;
 
 class Solution48 {
     public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> dic = new HashMap<>();
+        // Record the character and its last occurrence index
+        Map<Character,Integer> dic = new HashMap<>();
+
+        //Ensure that there are no duplicate characters in the index (i,j]
         int i = -1, res = 0;
         for(int j = 0; j < s.length(); j++) {
-            if(dic.containsKey(s.charAt(j)))
-                i = Math.max(i, dic.get(s.charAt(j))); // 更新左指针 i
-            dic.put(s.charAt(j), j); // 哈希表记录
-            res = Math.max(res, j - i); // 更新结果
+            char c = s.charAt(j);
+            if(dic.containsKey(c))
+                i = Math.max(i, dic.get(c));
+            dic.put(c, j);
+            res = Math.max(res, j - i);
         }
         return res;
     }
