b3fb09bb7fd9faa41465527bd5afa7e452b596f0
LA4AM12 <xingxiu.fyc@gmail.com>
2021-10-28 10:20:28 +0800

    3Sum -- medium

--- java_v/src/TowPointers/Solution15.java
+++ java_v/src/TowPointers/Solution15.java
@@ -4,52 +4,47 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * @author : LA4AM12
- * @create : 2021-10-26 15:50:59
- * @description : 3Sum
- */
 public class Solution15 {
 	public List<List<Integer>> threeSum(int[] nums) {
 		int n = nums.length;
 		List<List<Integer>> res = new ArrayList<>();
 
-		// make nums[fir] <= nums[snd] <= nums[trd]
+		// Remove duplicates by sorting
 		Arrays.sort(nums);
 
 		for (int fir = 0; fir < n-2; fir++) {
+			if (nums[fir] > 0) break;
+			// Remove duplicates
 			if (fir > 0 && nums[fir] == nums[fir-1]){
 				continue;
 			}
 
-			int snd = fir + 1;
-			int trd = n - 1;
+			// two pointers
+			int snd = fir + 1, trd = n - 1;
 
 			while (snd < trd){
-				if(snd > fir+1 && nums[snd] == nums[snd-1]){
+				// Remove duplicates
+				if(snd > fir + 1 && nums[snd] == nums[snd - 1]){
+					snd++;
 					continue;
 				}
-			}
+				int sum = nums[fir] + nums[snd] + nums[trd];
 
-			for (int snd = fir + 1; snd < n-1; snd++) {
-				if(snd > fir+1 && nums[snd] == nums[snd-1]){
-					continue;
-				}
-
-				while (snd < trd && nums[fir] + nums[snd] + nums[trd] > 0){
+				if(sum < 0){
+					snd++;
+				}else if (sum > 0){
 					trd--;
-				}
-
-				if(snd == trd) break;
-
-				if (nums[fir] + nums[snd] + nums[trd] == 0) {
+				}else {
 					List<Integer> list = new ArrayList<>();
 					list.add(nums[fir]);
 					list.add(nums[snd]);
 					list.add(nums[trd]);
 					res.add(list);
+					snd++;
+					trd--;
 				}
 			}
+
 		}
 		return res;
 	}
