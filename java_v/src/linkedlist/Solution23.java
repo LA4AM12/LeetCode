12b72ff62d931f1a7eea6ee50cb39a48308e0290
LA4AM12 <xingxiu.fyc@gmail.com>
2021-11-01 10:14:54 +0800

    Merge k Sorted Lists -- hard

---
+++ java_v/src/linkedlist/Solution23.java
@@ -0,0 +1,50 @@
+package linkedlist;
+
+import Offer.struct.ListNode;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * @author : LA4AM12
+ * @create : 2021-10-31 13:54:46
+ * @description : Merge k Sorted Lists
+ */
+public class Solution23 {
+	static class Entry implements Comparable<Entry>{
+		ListNode node;
+		int value;
+
+		public Entry(ListNode node) {
+			this.node = node;
+			value = node.val;
+		}
+
+		@Override
+		public int compareTo(Entry o) {
+			return this.value-o.value;
+		}
+	}
+
+	public ListNode mergeKLists(ListNode[] lists) {
+		Queue<Entry> queue = new PriorityQueue<>();
+
+		for (ListNode node : lists) {
+			if (node != null)
+				queue.offer(new Entry(node));
+		}
+
+		ListNode head = new ListNode(-1);
+		ListNode tail = head;
+
+		while (!queue.isEmpty()){
+			Entry entry = queue.poll();
+			tail.next = entry.node;
+			tail = tail.next;
+			if (entry.node.next != null)
+				queue.offer(new Entry(entry.node.next));
+		}
+
+		return head.next;
+	}
+}
