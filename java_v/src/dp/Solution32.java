92e5b1a40ff7b65b0783d29e2bcd38f7ae5f861b
LA4AM12 <xingxiu.fyc@gmail.com>
2021-11-09 19:54:51 +0800

    Longest Valid Parentheses -- hard

---
+++ java_v/src/dp/Solution32.java
@@ -0,0 +1,29 @@
+package dp;
+
+/**
+ * @author : LA4AM12
+ * @create : 2021-11-09 14:09:53
+ * @description : Longest Valid Parentheses
+ */
+public class Solution32 {
+	public int longestValidParentheses(String s) {
+		int len = s.length();
+		int ans = 0;
+
+		//dp[i] is the ans of substring of s end of s[i]
+		int[] dp = new int[len];
+
+		for (int i = 1; i < len; i++) {
+			if (s.charAt(i) == ')') {
+				if (s.charAt(i - 1) == '(') {
+					dp[i] = i - 2 >= 0 ? dp[i - 2] + 2 : 2;
+				} else if (i - 1 - dp[i - 1] >= 0 && s.charAt(i - 1 - dp[i - 1]) == '(') {
+					int pre = i - 2 - dp[i - 1] >= 0 ? dp[i - 2 - dp[i - 1]] : 0;
+					dp[i] = dp[i - 1] + 2 + pre;
+				}
+				ans = Math.max(ans, dp[i]);
+			}
+		}
+		return ans;
+	}
+}
