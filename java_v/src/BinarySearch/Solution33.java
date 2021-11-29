7aea5dffbe16a99f00a4bfcd4a04cac90c8bbf39
LA4AM12 <xingxiu.fyc@gmail.com>
2021-11-29 10:54:13 +0800

    Search in Rotated Sorted Array -- Medium

--- java_v/src/BinarySearch/Solution33.java
+++ java_v/src/BinarySearch/Solution33.java
@@ -6,24 +6,24 @@ package BinarySearch;
  * @description : Search in Rotated Sorted Array
  */
 public class Solution33 {
+	// log(n) bs
 	public int search(int[] nums, int target) {
-		int left = 0, right = nums.length - 1;
+		int l = 0, r = nums.length - 1;
 
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			if (target == nums[mid])
-				return mid;
-
-			if (nums[mid] < nums[0]) {
-				if (nums[mid] < target && target <= nums[nums.length - 1])
-					left = mid + 1;
+		while (l <= r) {
+			int m = (l + r) >>> 1;
+			if (target == nums[m])
+				return m;
+			if (nums[m] < nums[0]) {
+				if (nums[m] < target && target <= nums[r])
+					l = m + 1;
 				else
-					right = mid - 1;
+					r = m - 1;
 			} else {
-				if (nums[0] <= target && target < nums[mid])
-					right = mid - 1;
+				if (nums[l] <= target && target < nums[m])
+					r = r - 1;
 				else
-					left = mid + 1;
+					l = m + 1;
 			}
 		}
 		return -1;
