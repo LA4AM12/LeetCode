7473c530e466956dd05d8006e260a3bb1dc574fe
LA4AM12 <xingxiu.fyc@gmail.com>
2021-12-23 16:12:21 +0800

    Binary Tree Inorder Traversal -- Easy

---
+++ go_v/src/easy/Inorder Traversal.go
@@ -0,0 +1,29 @@
+package easy
+
+import "container/list"
+
+// leetcode-0226 2021/12/23
+
+func inorderTraversal(root *TreeNode) []int {
+	ans := make([]int, 0)
+
+	if root == nil {
+		return ans
+	}
+
+	stack := list.New()
+	cur := root
+
+	for cur != nil || stack.Len() > 0 {
+		if cur != nil {
+			stack.PushFront(cur)
+			cur = cur.Left
+		} else {
+			cur = stack.Remove(stack.Front()).(*TreeNode)
+			ans = append(ans, cur.Val)
+			cur = cur.Right
+		}
+	}
+
+	return ans
+}
