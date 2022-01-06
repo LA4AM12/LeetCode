1dd962b0475d7126a58aa58062e4e5ae720d4965
fengyicheng <fengyicheng.412@bytedance.com>
2022-01-06 23:53:47 +0800

    Restore IP Addresses - Medium

--- java_v/src/backtracking/Solution93.java
+++ java_v/src/backtracking/Solution93.java
@@ -9,49 +9,32 @@ import java.util.List;
  * @description : Restore IP Addresses
  */
 public class Solution93 {
-	static final int SEG_COUNT = 4;
-	List<String> ans = new ArrayList<>();
-	int[] segments = new int[SEG_COUNT];
-	int len;
+    List<String> ans = new ArrayList<>();
+    String[] IP = new String[4];
 
-	public List<String> restoreIpAddresses(String s) {
-		len = s.length();
-		dfs(s, 0, 0);
-		return ans;
-	}
+    public List<String> restoreIpAddresses(String s) {
+        backtrack(s, 0, 0);
+        return ans;
+    }
 
-	public void dfs(String s, int segId, int segStart) {
-		//剪枝
-		if (len - segStart < SEG_COUNT - segId || len - segStart > 3 * (SEG_COUNT - segId))
-			return;
+    private void backtrack(String s, int cur, int seg) {
+        // cut
+        if (s.length() - cur < 4 - seg || s.length() - cur > 3 * (4 - seg))
+            return;
+        if (seg == 4) {
+            ans.add(IP[0] + "." + IP[1] + "." + IP[2] + "." + IP[3]);
+            return;
+        }
 
-		//结束条件
-		if (segId == SEG_COUNT) {
-			StringBuilder ip = new StringBuilder();
-			for (int i = 0; i < SEG_COUNT; i++) {
-				ip.append(segments[i]);
-				if (i != SEG_COUNT - 1)
-					ip.append('.');
-			}
-			ans.add(ip.toString());
-			return;
-		}
-
-		// 由于不能有前导零，如果当前数字为 0，那么这一段 IP 地址只能为 0
-		if (s.charAt(segStart) == '0') {
-			segments[segId] = 0;
-			dfs(s, segId + 1, segStart + 1);
-		} else {
-			int seg = 0;
-			for (int i = segStart; i < len; i++) {
-				seg = seg * 10 + (s.charAt(i) - '0');
-				if (seg <= 255) {
-					segments[segId] = seg;
-					dfs(s, segId + 1, i + 1);
-				} else {
-					break;
-				}
-			}
-		}
-	}
+        // 切割
+        for (int i = 1; cur + i <= s.length(); i++) {
+            IP[seg] = s.substring(cur, cur + i);
+            // 不能存在前导0
+            if (i != 1 && IP[seg].charAt(0) == '0')
+                break;
+            if (Integer.parseInt(IP[seg]) > 255)
+                break;
+            backtrack(s, cur + i, seg + 1);
+        }
+    }
 }
