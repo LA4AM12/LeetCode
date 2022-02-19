600d43aed75f415e202e6e63e0927f7d96ce5296
fengyicheng <fengyicheng.412@bytedance.com>
2022-02-19 18:08:41 +0800

    Combination Sum IV -- Medium

---
+++ java_v/src/dp/Solution377.java
@@ -0,0 +1,21 @@
+package dp;
+
+/**
+ * @author : LA4AM12
+ * @create : 2022-02-19 17:45:52
+ * @description : Combination Sum IV
+ */
+public class Solution377 {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
