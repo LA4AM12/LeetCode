9e2265051f0c8013657027160a1ac24aaa724076
LA4AM12 <xingxiu.fyc@gmail.com>
2022-03-16 17:48:01 +0800

    Coin Change 2 -- Medium

--- java_v/src/dp/Solution518.java
+++ java_v/src/dp/Solution518.java
@@ -1,20 +1,27 @@
 package dp;
 
+/**
+ * @author : LA4AM12
+ * @create : 2021-10-04 11:09:25
+ * @description : Coin Change 2
+ */
+
 public class Solution518 {
-    public int change(int amount, int[] coins) {
-        //dp[i][j]
-        int[] dp = new int[amount+1];
+	public int change(int amount, int[] coins) {
+		//dp[i][j] = dp[i-1][j] + dp[i][j-coin]
+		int[] dp = new int[amount + 1];
 
-        //base case  dp[...][0] = 1
-        dp[0] = 1;
-        for (int i = 1; i <= coins.length; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (j-coins[i-1] >= 0) {
-                    dp[j] += dp[j-coins[i-1]];
-                }
-            }
-        }
+		//base case  dp[...][0] = 1
+		dp[0] = 1;
 
-        return dp[amount];
-    }
+        // combinations
+		for (int coin : coins) {
+			for (int j = 1; j <= amount; j++) {
+				if (j - coin >= 0) {
+					dp[j] += dp[j - coin];
+				}
+			}
+		}
+		return dp[amount];
+	}
 }
